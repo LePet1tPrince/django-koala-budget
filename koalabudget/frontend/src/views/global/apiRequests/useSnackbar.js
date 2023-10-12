11d81a235fab445ae3e4090759b5eaf3c8085f35
lepetitprince <TimmyLBender@gmail.com>
2023-10-12 15:48:06 -0400

    created a useSnackbar custom hook. has the state persistance issue

---
+++ koalabudget/frontend/src/views/global/apiRequests/useSnackbar.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect} from 'react';
+import SimpleSnackbar from '../SimpleSnackbar';
+
+function useSnackbar() {
+    const [snackbarData, setSnackbarData] = useState({
+      })
+
+
+    function openSnackbar(message, severity) {
+        setSnackbarData({ message: message, severity: severity, isOpen: true})
+    }
+
+
+
+    const SnackbarComponent = () => {
+    return <SimpleSnackbar snackbarData={snackbarData} setSnackbarData={setSnackbarData} />}
+    
+    
+  return {SnackbarComponent, openSnackbar}
+}
+
+export default useSnackbar
--- koalabudget/frontend/src/views/tests/TestView.jsx
+++ koalabudget/frontend/src/views/tests/TestView.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import TestFetchThen from './TestFetchThen';
+import useSnackbar from '../global/apiRequests/useSnackbar';
+import Button from '@mui/material/Button'
 
 function TestView() {
+  const {SnackbarComponent, openSnackbar} = useSnackbar()
+
+ 
   return (
     <div>
-        <TestFetchThen/>
+        {/* <TestFetchThen/> */}
+        <SnackbarComponent/>
+        <Button variant="outlined" onClick={() =>openSnackbar("This is my message","success")}>setOpen</Button>
         
     </div>
   )
