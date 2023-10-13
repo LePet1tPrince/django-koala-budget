d5f9cf8b83483aaa6c253d34e01eaff52d57d761
lepetitprince <TimmyLBender@gmail.com>
2023-10-13 15:57:09 -0400

    added input fields for budget with state

--- koalabudget/frontend/src/views/budget/reports/BudgetTable.jsx
+++ koalabudget/frontend/src/views/budget/reports/BudgetTable.jsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { useState } from 'react';
+// import * as React from 'react';
+import react, { useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,10 +9,13 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Typography } from '@mui/material';
 import { DollarFormat } from '../../global/apiRequests/global';
+import TextField from '@mui/material/TextField';
+
 
 
 
 export default function BudgetTable({ budget }) {
+  const [changedData, setChangedData] = useState([...budget])
   // const [totals, setTotals] = useState([])
   let budget_total = 0;
   let actual_total = 0;
@@ -24,6 +27,41 @@ export default function BudgetTable({ budget }) {
     actual_total += parseFloat(row.actual);
     available_total += parseFloat(row.available);
   })
+
+  // function handleChange(e, id) {
+  //   // setChangedData([...changedData, id: e.target.value])
+  //    const newdata = [...changedData]
+  //    console.log("newdata", newdata)
+  //    const changedRow = newdata.filter(row => row.id === id)[0]
+  //    console.log("changedrow", changedRow)
+
+  //    const unchangedRows = newdata.filter(row => row.id !== id)
+
+  //    setChangedData([...unchangedRows, {...changedRow, budget: e.target.value}])
+  //    console.log([...unchangedRows, {...changedRow, budget: e.target.value}])
+
+
+  // }
+
+  const handleChange = (e, row) => {
+
+      const updatedData = changedData.map((data) => {
+        if (data.id === row.id) {
+          // Update the 'budget' field of the matching row with the new value
+          const currentDecimals = e.target.value.toString().split('.')[1]?.length
+          return { ...data, budget: parseFloat(e.target.value).toFixed(Math.min(currentDecimals,2)) };
+        }
+        return data;
+      });
+      
+      setChangedData(updatedData);
+    };
+
+    function handleBlur() {
+      console.log("BLURURURU")
+    }
+
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -37,15 +75,26 @@ export default function BudgetTable({ budget }) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {budget?.map((row) => (
+          {changedData?.sort((a,b) => a.num - b.num).map((row) => (
             <TableRow
-              key={row.name}
+              key={row.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row.category.name} - {row.category.num}
               </TableCell>
-              <TableCell align="right">{DollarFormat.format(row.budget)}</TableCell>
+              <TableCell align="right">
+
+                <TextField
+                  type="number"
+                  margin="none"
+                  onChange={(e) => handleChange(e,row)}
+                  value={row.budget}
+                  onBlur={handleBlur}
+                  />
+              </TableCell>
+
+              {/* <TableCell align="right">{DollarFormat.format(row.budget)}</TableCell> */}
               <TableCell align="right">{DollarFormat.format(row.actual)}</TableCell>
               <TableCell align="right">{DollarFormat.format(row.available)}</TableCell>
 
