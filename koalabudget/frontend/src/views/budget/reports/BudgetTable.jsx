debe9f16643e351780ca28ac05b83c13a01b5338
lepetitprince <TimmyLBender@gmail.com>
2023-11-13 20:08:54 -0500

    added accordion list of trasnactions to month budget

--- koalabudget/frontend/src/views/budget/reports/BudgetTable.jsx
+++ koalabudget/frontend/src/views/budget/reports/BudgetTable.jsx
@@ -1,8 +1,14 @@
 import { calculateTotals, reverseBudgetValues, sortBudget } from '../../global/functions/BudgetFunctions';
 import react, { useEffect, useState } from 'react';
 
+import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
+import CircularProgress from '@mui/material/CircularProgress';
+import Collapse from '@mui/material/Collapse';
 import { DollarFormat } from '../../global/apiRequests/global';
+import IconButton from '@mui/material/IconButton';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Paper from '@mui/material/Paper';
 import SimpleSnackbar from '../../global/components/SimpleSnackbar';
 import Table from '@mui/material/Table';
@@ -13,8 +19,110 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
 import { Typography } from '@mui/material';
+import dayjs from 'dayjs';
 import { putBudget } from '../../global/apiRequests/budget';
+import useFetch from '../../global/customHooks/useFetch';
 import useSnackbar from '../../global/customHooks/useSnackbar';
+
+function Row(props) {
+  const { row, handleChange, handleBlur, transactions, isTransactionsLoading } = props;
+  const [open, setOpen] = useState(false)
+  
+  const filteredTransactions = transactions?.filter(trxn => (trxn.debit.id === row.category.id || trxn.credit.id === row.category.id)&& dayjs(row.month, "YYYY-MM-DD").format("YYYY-MM") === dayjs(trxn.date,"YYYY-MM-DD").format("YYYY-MM"))
+  
+
+  return (
+    <>
+    {/* {JSON.stringify(dayjs(row.month, "YYYY-MM-DD").format("YYYY-MM"))} */}
+    {/* {JSON.stringify(transactions)} */}
+    <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 }, "padding": "0" }}
+            >
+              <TableCell>
+              <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+            disabled={filteredTransactions?.length === 0}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {row.category.name}
+              </TableCell>
+              <TableCell align="right"
+              sx={{"margin": "0px", "padding": "0"}}>
+
+                <TextField
+                  type="number"
+                  onChange={(e) => handleChange(e,row)}
+                  value={row.budget}
+                  onBlur={() => handleBlur(row)}
+                  size="small"
+                  
+                  />
+              </TableCell>
+
+              {/* <TableCell align="right">{DollarFormat.format(row.budget)}</TableCell> */}
+              <TableCell align="right">
+              <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+            disabled={filteredTransactions?.length === 0}
+          >
+            {/* {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />} */}
+                {DollarFormat.format(row.actual)}
+          </IconButton>
+                </TableCell>
+              <TableCell align="right">
+              <Chip label={DollarFormat.format(row.available)} color={row.available>=0?'success':'error'} />
+              {/* <Button onClick={handleBlur} variant="contained">Save</Button> */}
+              </TableCell>
+
+            </TableRow>
+            <TableRow>
+            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <h2>Transactions</h2>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Amount</TableCell>
+                  <TableCell>To account</TableCell>
+                  <TableCell>Notes</TableCell>
+                </TableRow>
+              </TableHead>
+            {isTransactionsLoading? 
+            <TableRow sx={{ 'margin': 'auto' }}>
+              <TableCell width='100%'>
+              <CircularProgress />
+
+              </TableCell>
+            </TableRow>:
+              <TableBody>
+                {filteredTransactions?.map(trxn => {
+                  return (
+                    <TableRow key={trxn.id}>
+                      <TableCell>{trxn.date}</TableCell>
+                      <TableCell>{trxn.amount}</TableCell>
+                      <TableCell>{trxn.debit.id === row.category.id? trxn.credit.name:trxn.debit.name}</TableCell>
+                      <TableCell>{trxn.Notes}</TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+              }
+            </Table>
+            </Collapse>
+
+          </TableCell>
+            </TableRow>
+            </>
+  )
+}
 
 export default function BudgetTable(props) {
   const { budget, budgetThisMonth, tableType } = props;
@@ -33,6 +141,7 @@ export default function BudgetTable(props) {
   const {snackbarData, setSnackbarData, openSnackbar} = useSnackbar();
   const debugSetting = localStorage.getItem('debugSetting');
   const [columnTotals, setColumnTotals] = useState({budget: 0, actual: 0, available: 0});
+  const [ transactions, setTransactions, isTransactionsLoading, isTransactionsError] = useFetch(`/transactions`)
 
 
 
@@ -146,34 +255,15 @@ export default function BudgetTable(props) {
         </TableHead>
         <TableBody>
           {sortBudget(changedData)?.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 }, "padding": "0" }}
-            >
-              <TableCell component="th" scope="row">
-                {row.category.name}
-              </TableCell>
-              <TableCell align="right"
-              sx={{"margin": "0px", "padding": "0"}}>
-
-                <TextField
-                  type="number"
-                  onChange={(e) => handleChange(e,row)}
-                  value={row.budget}
-                  onBlur={() => handleBlur(row)}
-                  size="small"
-                  
-                  />
-              </TableCell>
-
-              {/* <TableCell align="right">{DollarFormat.format(row.budget)}</TableCell> */}
-              <TableCell align="right">{DollarFormat.format(row.actual)}</TableCell>
-              <TableCell align="right">
-              <Chip label={DollarFormat.format(row.available)} color={row.available>=0?'success':'error'} />
-              {/* <Button onClick={handleBlur} variant="contained">Save</Button> */}
-              </TableCell>
-
-            </TableRow>
+            <Row key={row.name}
+             row={row} 
+             handleChange={handleChange} 
+             handleBlur={handleBlur} 
+             transactions={transactions}
+             isTransactionsLoading={isTransactionsLoading}
+             />
+            
+            
           ))}
           <TableRow>
           <TableCell><Typography variant="h5">Total</Typography></TableCell>
